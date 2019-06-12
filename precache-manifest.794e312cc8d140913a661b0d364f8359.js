a673a266d7387617c5fcb253abddeaa6d42349ab
Lanka <darshaka4mail@gmail.com>
2019-06-13 00:56:52 +0530

    Updates

---
+++ precache-manifest.794e312cc8d140913a661b0d364f8359.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c759d17436a61fe70c04853c0b2ee26d",
+    "url": "/litmuslab-deploy/index.html"
+  },
+  {
+    "revision": "587eb1f254b665805438",
+    "url": "/litmuslab-deploy/static/css/main.86b46a33.chunk.css"
+  },
+  {
+    "revision": "e3e037909df35b505e01",
+    "url": "/litmuslab-deploy/static/js/2.6e858921.chunk.js"
+  },
+  {
+    "revision": "587eb1f254b665805438",
+    "url": "/litmuslab-deploy/static/js/main.bc3a8f84.chunk.js"
+  },
+  {
+    "revision": "6932a14a3e6d4385205f",
+    "url": "/litmuslab-deploy/static/js/runtime~main.3f47d730.js"
+  },
+  {
+    "revision": "41e145443e4b40384c3f41b41772ca71",
+    "url": "/litmuslab-deploy/static/media/banner.41e14544.png"
+  },
+  {
+    "revision": "651039bc4d18dc9b04ee926b23cbbca6",
+    "url": "/litmuslab-deploy/static/media/banner1.651039bc.png"
+  },
+  {
+    "revision": "76845dde6ddf4ec00c128838ba965e03",
+    "url": "/litmuslab-deploy/static/media/banner2.76845dde.png"
+  },
+  {
+    "revision": "013aef7f7adefac6213a43aa9f4961cc",
+    "url": "/litmuslab-deploy/static/media/banner3.013aef7f.png"
+  }
+]);
